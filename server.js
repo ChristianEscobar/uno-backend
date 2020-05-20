ff9a3a294fc5cf7025dc6ce617c2a0902c51650a
Christian Escobar <crs.escobar@gmail.com>
2020-05-19 21:30:14 -0700

    add body-parser middleware

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { routes } = require('./routes');
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const port = process.env.PORT || 3000;
 
 const start = function start() {
