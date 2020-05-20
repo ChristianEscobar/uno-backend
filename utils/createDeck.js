c80571a3c0914ee99909848654df14dd278248a8
Christian Escobar <crs.escobar@gmail.com>
2020-05-19 21:29:59 -0700

    create helper functions

---
+++ utils/createDeck.js
@@ -0,0 +1,56 @@
+const colors = ['red', 'yellow', 'green', 'blue'];
+const values = [
+	'0',
+	'1',
+	'1',
+	'2',
+	'2',
+	'3',
+	'3',
+	'4',
+	'4',
+	'5',
+	'5',
+	'6',
+	'6',
+	'7',
+	'7',
+	'8',
+	'8',
+	'9',
+	'9',
+	'SKIP',
+	'SKIP',
+	'REVERSE',
+	'REVERSE',
+	'DRAW_TWO',
+	'DRAW_TWO',
+	'WILD',
+	'WILD_DRAW_FOUR',
+];
+
+const createDeck = function createDeck() {
+	const deck = [];
+	let cardCounter = 0;
+
+	colors.forEach((color) => {
+		values.forEach((value) => {
+			cardCounter++;
+			deck.push(createCard(cardCounter, color, value));
+		});
+	});
+
+	return deck;
+};
+
+const createCard = function createCard(seqNum, color, value) {
+	return {
+		seqNum,
+		color,
+		value,
+	};
+};
+
+module.exports = {
+	createDeck,
+};
---
+++ utils/shuffleDeck.js
@@ -0,0 +1,9 @@
+const Shuffle = require('shuffle');
+
+const shuffleDeck = function shuffledDeck(deck) {
+	return Shuffle.shuffle({ deck });
+};
+
+module.exports = {
+	shuffleDeck,
+};
