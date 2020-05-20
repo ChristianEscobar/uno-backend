8de868b052581a39aa5260b27da2e711acb855b1
Christian Escobar <crs.escobar@gmail.com>
2020-05-19 21:30:23 -0700

    add routes

--- routes.js
+++ routes.js
@@ -1,7 +1,20 @@
 const routes = require('express').Router();
+const { createDeck } = require('./utils/createDeck');
+const { shuffleDeck } = require('./utils/shuffleDeck');
 
 routes.get('/ping', (req, res) => {
 	res.status(200).json({ message: 'Pong! Application is up and running' });
+});
+
+routes.get('/createDeck', (req, res) => {
+	const deck = createDeck();
+	res.status(200).json({ deck, totalCards: deck.length });
+});
+
+routes.get('/shuffleDeck', (req, res) => {
+	const shuffledDeckObj = shuffleDeck(req.body.deck);
+	const { cards: shuffledDeck } = shuffledDeckObj;
+	res.status(200).json({ shuffledDeck });
 });
 
 module.exports = { routes };
