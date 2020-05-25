42c4cb96dec0e81de7b1920e7c6562ef0ed9e000
Christian Escobar <crs.escobar@gmail.com>
2020-05-24 20:06:57 -0700

    update deck database controller

--- controllers/database/DeckDbController.js
+++ controllers/database/DeckDbController.js
@@ -4,7 +4,19 @@ const Deck = require('../../models/Deck');
 
 class DeckDbController {
 	async addDeck(deck) {
-		return Deck.insertMany(deck);
+		try {
+			await Deck.insertMany(deck);
+		} catch (err) {
+			throw new Error(`Error encountered on insert: ${err.message}`);
+		}
+	}
+
+	async deleteDeck() {
+		try {
+			await Deck.deleteMany({});
+		} catch (err) {
+			throw new Error(`Error encountered on delete: ${err.message}`);
+		}
 	}
 }
 
