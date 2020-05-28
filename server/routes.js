7ce882f3735c4b9aee4affcb4a8195a1891fffcf
Christian Escobar <crs.escobar@gmail.com>
2020-05-27 19:48:23 -0700

    add routes for players

--- server/routes.js
+++ server/routes.js
@@ -3,7 +3,10 @@
 const routes = require('express').Router();
 const deckController = require('../controllers/routes/deckController');
 const gameController = require('../controllers/routes/gameController');
+const playerController = require('../controllers/routes/playerController');
 
+routes.post('/players', playerController.add_player);
+routes.get('/players/playing/:playing', playerController.get_players);
 routes.post('/startGame', gameController.start_game);
 
 // routes.get('/createDeck', deckController.create_deck_get);
